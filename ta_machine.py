000b86f9945ca556bb582cc85383035290af7836
Bana Abraham <bana.abraham@gmail.com>
2017-09-05 17:09:37 +0700

    Update ta_machine.py

--- ta_machine.py
+++ ta_machine.py
@@ -41,24 +41,26 @@ def prediksi(ticker):
     frames = [df,rsi,atr,roc,wilr,mom,aaron]
     data = pd.concat(frames,axis=1)
     data = data.loc[14:,~data.columns.duplicated()].reset_index()
-    data = data.dropna()
+    data = data.dropna(how='any')
     close = data['Close'].values
     
     Target = []
     for i in range(len(close)):
         try:
-            if close[i]<close[i+1]:
+            if (close[i]*1.01)<close[i+1]:
                 Target.append("UP")
-            else:
+            elif (close[i]*0.99)<=close[i+1] and (close[i]*1.01)>=close[i+1]:
+                Target.append("STABLE")
+            elif (close[i]*0.99)>close[i+1]:
                 Target.append("DOWN")
+                
         except:
             Target.append("-")         
-    
     
     forest = RandomForestClassifier(n_estimators=5, random_state=2)  
     X=data[['RSI_14','ATR_14','ROC_14','WILLR',"MOM_14","ARN_14"]].copy()
     train_X = X[:len(X)-1]
-    train_Y = Target[:len(Target)-1] #.reshape(len(Target),)
+    train_Y = Target[:len(Target)-1] 
     forest.fit(train_X,train_Y)
     
     RandomForestClassifier(bootstrap=True, class_weight=None, criterion='gint', max_depth=None, max_features='auto', max_leaf_nodes=None,min_samples_leaf=1,min_samples_split=2,min_weight_fraction_leaf=0.0, n_estimators=100,n_jobs=1,oob_score=False, random_state=2, verbose= 2, warm_start=False)
@@ -103,4 +105,4 @@ def prediksi(ticker):
 
 if __name__ == '__main__':
     ticker = input("predict what stock?: ")
-    y=prediksi(ticker)
+    prediksi(ticker)
